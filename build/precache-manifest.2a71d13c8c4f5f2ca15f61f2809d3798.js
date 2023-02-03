9d219cc87baedd47e58e6fa836d3fc6a17d2c330
Anish Kumar <21U01053@iiitbhopal.ac.in>
2023-02-03 17:05:44 +0530

    Add files via upload

---
+++ build/precache-manifest.2a71d13c8c4f5f2ca15f61f2809d3798.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca085f9bc59833cd7a0fcd537286c1fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85ab78f05f86b28f1bf7",
+    "url": "/static/css/main.c5909856.chunk.css"
+  },
+  {
+    "revision": "821341f4c905c24da4d2",
+    "url": "/static/js/0.09caffca.chunk.js"
+  },
+  {
+    "revision": "5501c485481b4eb0b454",
+    "url": "/static/js/1.a1a129b2.chunk.js"
+  },
+  {
+    "revision": "75ca5abce6064f7f8350",
+    "url": "/static/js/10.92df0613.chunk.js"
+  },
+  {
+    "revision": "77cfd4d336a6564f1482",
+    "url": "/static/js/11.ef64bfa8.chunk.js"
+  },
+  {
+    "revision": "6b559747c265846e93bc",
+    "url": "/static/js/12.55f0dd8e.chunk.js"
+  },
+  {
+    "revision": "ea21ccf75e19eb5c3812",
+    "url": "/static/js/13.87dfd073.chunk.js"
+  },
+  {
+    "revision": "96e368b6ef51023d5200",
+    "url": "/static/js/14.553d4f58.chunk.js"
+  },
+  {
+    "revision": "b5c95230c224196d2204",
+    "url": "/static/js/15.2db4fa05.chunk.js"
+  },
+  {
+    "revision": "525365523035e87cfc08",
+    "url": "/static/js/16.38d97a6f.chunk.js"
+  },
+  {
+    "revision": "5a9d90e2153ec1fa2ca0",
+    "url": "/static/js/17.8f194e20.chunk.js"
+  },
+  {
+    "revision": "caf4e6092bbf04010cb5",
+    "url": "/static/js/18.a42457a5.chunk.js"
+  },
+  {
+    "revision": "6a07b208e1cfac49ad48",
+    "url": "/static/js/19.03b0e638.chunk.js"
+  },
+  {
+    "revision": "aa7568f646a73dde8a28",
+    "url": "/static/js/2.94622325.chunk.js"
+  },
+  {
+    "revision": "074e52b1b4b0fd0288a5",
+    "url": "/static/js/20.a669d424.chunk.js"
+  },
+  {
+    "revision": "29c0dc5695466ff613ba65f5bf835499",
+    "url": "/static/js/20.a669d424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3667f3ecf2398c6dbc2",
+    "url": "/static/js/21.2b702878.chunk.js"
+  },
+  {
+    "revision": "d42b9f7e6eed27b5ea20",
+    "url": "/static/js/22.3c654fe6.chunk.js"
+  },
+  {
+    "revision": "65011e828e111d7d245a",
+    "url": "/static/js/23.5a3e5b08.chunk.js"
+  },
+  {
+    "revision": "1c1213c913d8a0d4068bc13c83f7e53c",
+    "url": "/static/js/23.5a3e5b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c82295e0caf4e3b6430b",
+    "url": "/static/js/24.88a467c0.chunk.js"
+  },
+  {
+    "revision": "7daad90f5a1f35391ae1",
+    "url": "/static/js/25.20a9680f.chunk.js"
+  },
+  {
+    "revision": "d80b967e69abf1daef08",
+    "url": "/static/js/26.84319c57.chunk.js"
+  },
+  {
+    "revision": "fab7941778d649079314",
+    "url": "/static/js/27.3550d74a.chunk.js"
+  },
+  {
+    "revision": "3d04757fd009a55e054c",
+    "url": "/static/js/28.1b0332cb.chunk.js"
+  },
+  {
+    "revision": "a70d87090e4e61bff603",
+    "url": "/static/js/29.f13f9bf8.chunk.js"
+  },
+  {
+    "revision": "b4fbe2b59f41a021fbbd",
+    "url": "/static/js/30.31cd24fa.chunk.js"
+  },
+  {
+    "revision": "e129692dbbd7507a57d3",
+    "url": "/static/js/31.365f4a06.chunk.js"
+  },
+  {
+    "revision": "a61213d10a88393cabc1",
+    "url": "/static/js/32.f8b3b47f.chunk.js"
+  },
+  {
+    "revision": "e7d9b8c1d195db1bc830",
+    "url": "/static/js/33.71128a59.chunk.js"
+  },
+  {
+    "revision": "ceca4a164f9c68a78b06",
+    "url": "/static/js/34.de893a87.chunk.js"
+  },
+  {
+    "revision": "c850fc4ec49b99bccf34",
+    "url": "/static/js/5.4dbc0007.chunk.js"
+  },
+  {
+    "revision": "28b1fb3267bed1dc4b7d045da087b83a",
+    "url": "/static/js/5.4dbc0007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0302734177ff83f3fbca",
+    "url": "/static/js/6.1ffb676b.chunk.js"
+  },
+  {
+    "revision": "bf9f179ebcbf53b797af01d432e4ced8",
+    "url": "/static/js/6.1ffb676b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965063f0baedf07d36ba",
+    "url": "/static/js/7.547d8e5a.chunk.js"
+  },
+  {
+    "revision": "086ac0f260c0d88cf399717695349565",
+    "url": "/static/js/7.547d8e5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83dc464c8f08388dc7b",
+    "url": "/static/js/8.53a9179e.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/8.53a9179e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d6311ba43dfd1e3346",
+    "url": "/static/js/9.84358769.chunk.js"
+  },
+  {
+    "revision": "85ab78f05f86b28f1bf7",
+    "url": "/static/js/main.12b1ba36.chunk.js"
+  },
+  {
+    "revision": "3a2a6d8a1838ce978b29",
+    "url": "/static/js/runtime-main.241ae471.js"
+  },
+  {
+    "revision": "3761c6a5e09a70682a2debdf67997375",
+    "url": "/static/media/background.3761c6a5.jpg"
+  },
+  {
+    "revision": "6a97acfdd9a0fd67236a8316f7b03739",
+    "url": "/static/media/bbond.6a97acfd.png"
+  },
+  {
+    "revision": "084590b2472904a63ee00f8b5ebe68d3",
+    "url": "/static/media/bomb-bitcoin-LP.084590b2.png"
+  },
+  {
+    "revision": "37bb8ded59f79d3f6d479d987baf9cf2",
+    "url": "/static/media/bomb-logo.37bb8ded.png"
+  },
+  {
+    "revision": "2150b3258c6a290dd226cd2b1de3d945",
+    "url": "/static/media/bomb.2150b325.png"
+  },
+  {
+    "revision": "9144444a80125f1dd4bd40df2ad9fdef",
+    "url": "/static/media/bshare-bnb-LP.9144444a.png"
+  },
+  {
+    "revision": "3cc910aa0f0b77cf5eae4237ff78242b",
+    "url": "/static/media/bshares.3cc910aa.png"
+  },
+  {
+    "revision": "277a944dde4c96672f4196f551598e72",
+    "url": "/static/media/busm-256.277a944d.png"
+  },
+  {
+    "revision": "6ecf5a86eeaf5ba2ad60b44344cdaee1",
+    "url": "/static/media/busm-busd-lp.6ecf5a86.png"
+  },
+  {
+    "revision": "3765bf16134347a88f03141d2b07b18d",
+    "url": "/static/media/coinbase_logo.3765bf16.jpeg"
+  },
+  {
+    "revision": "d0c042e3e8733923d5aa22dfe170d6bb",
+    "url": "/static/media/discord.d0c042e3.svg"
+  },
+  {
+    "revision": "a56347f8dc0949546f8c1fe5a1d426a0",
+    "url": "/static/media/github.a56347f8.svg"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "a45b137b509f9c5766171f934f107ac5",
+    "url": "/static/media/telegram.a45b137b.svg"
+  },
+  {
+    "revision": "a333b07f78028b058b162c2f019809f7",
+    "url": "/static/media/twitter.a333b07f.svg"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/static/media/wallet-connect.37f2bc6e.svg"
+  },
+  {
+    "revision": "d3987eeead31d8ef4467dd57996e5632",
+    "url": "/static/media/xbomb.d3987eee.png"
+  }
+]);
